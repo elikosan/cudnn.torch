d55d55ce59397df7d0ad82f139eb43ae23845b1e
Eric Cosatto <cosatto@nec-labs.com>
2017-05-05 17:30:34 -0400

    accept cudnn 6

--- ffi.lua
+++ ffi.lua
@@ -1592,7 +1592,7 @@ if CUDNN_PATH then
     cudnn.C = ffi.load(CUDNN_PATH)
 else
 
-    local libnames = {'libcudnn.so.5', 'libcudnn.5.dylib', 'cudnn64_5.dll'}
+    local libnames = {'libcudnn.so.6', 'libcudnn.6.dylib', 'cudnn64_6.dll','libcudnn.so.5', 'libcudnn.5.dylib', 'cudnn64_5.dll'}
     local ok = false
     for i=1,#libnames do
         ok = pcall(function () cudnn.C = ffi.load(libnames[i]) end)
@@ -1614,8 +1614,8 @@ end
 
 -- check cuDNN version
 cudnn.version = tonumber(cudnn.C.cudnnGetVersion())
-if cudnn.version < 5005 or cudnn.version >= 6000 then
-   error('These bindings are for CUDNN 5.x (5005 <= cudnn.version > 6000) , '
+if cudnn.version < 5005 or cudnn.version >= 7000 then
+   error('These bindings are for CUDNN 5.x or 6.x (5005 >= cudnn.version < 7000) , '
         .. 'while the loaded CuDNN is version: ' .. cudnn.version
            .. '  \nAre you using an older or newer version of CuDNN?')
 end
